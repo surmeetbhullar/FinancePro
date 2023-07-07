4e28e2c92bc37539e8c6929e67b6c9932cbe0bc7
surmeetbhullar <99696937+surmeetbhullar@users.noreply.github.com>
2023-07-06 22:11:36 -0700

    Update FinanceProGPT.py

--- FinanceProGPT.py
+++ FinanceProGPT.py
@@ -3,7 +3,7 @@ import gradio
 
 openai.api_key = "####"
 
-messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+messages = [{"role": "system", "content": "You are a financial expert that advises customers on questions in regards to spending and savings."}]
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
@@ -15,6 +15,6 @@ def CustomChatGPT(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "FinancePro")
 
 demo.launch(share=True)
